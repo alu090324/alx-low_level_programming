45523e4b338333107fcebc82ccb30c52490d9150
alu090324 <aluwanindlovu25@gmail.com>
2023-08-12 06:41:04 -0700

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *printf - A program prints a line with the use of function printf not puts
+ *return: 0 A program will run with success
+ */
+
+int main(void)
+{
+pritnf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
