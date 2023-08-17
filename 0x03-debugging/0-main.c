3b6300ddbb3687e1f8ec0e8c3d60c0761c9e3490
alu090324 <aluwanindlovu25@gmail.com>
2023-08-17 08:46:19 -0700

    comment

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - Entry point, test function for
+ *       positive or negative
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return(0);
+}
