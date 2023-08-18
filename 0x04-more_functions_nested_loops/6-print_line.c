55e0b43a8b7048a21a1775427d83312b1faa3199
alu090324 <aluwanindlovu25@gmail.com>
2023-08-18 10:32:43 -0700

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line- Entry point
+ * @n: function that draws a line in terminal
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
