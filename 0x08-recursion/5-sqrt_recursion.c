c7d53b47e5e4c12ae55c28528b50f0447e6f00e6
alu090324 <aluwanindlovu25@gmail.com>
2023-08-31 07:50:13 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,8 +2,8 @@
 /**
  * sqrt_a- function that returns the
  * natural square root of a number.
- * @b:input number
- * @a:iterator
+ * @a:input number
+ * @b:iterator
  * Return: square root or -1
  */
 int sqrt_a(int a, int b)
