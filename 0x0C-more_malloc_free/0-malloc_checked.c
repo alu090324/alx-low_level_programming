b7b49fbe749dfeebceadeae4afc75695490983ac
alu090324 <aluwanindlovu25@gmail.com>
2023-09-07 05:30:36 -0700

    concatenate

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include<stdlib.h>
+/**
+ * malloc_checked-allocates memory using malloc.
+ * @b:number of bytes to be allocated
+ * Return:pointer of allocated block
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
