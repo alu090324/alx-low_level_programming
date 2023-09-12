110d772b58081f85fd6ed86df133db92dccff099
alu090324 <aluwanindlovu25@gmail.com>
2023-09-12 00:49:05 -0700

    dog t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,9 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t- typedef of struct dog
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
